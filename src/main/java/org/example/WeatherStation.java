a8d86a9beddd008d8a4c5510a75b5bf9668c2aec
moaz nabil <moaznabil886@gmail.com>
2023-05-01 19:05:06 +0300

    first commit

---
+++ src/main/java/org/example/WeatherStation.java
@@ -0,0 +1,54 @@
+package org.example;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class WeatherStation {
+    private static final String TOPIC = "weather_data";
+    private static final String BOOTSTRAP_SERVERS = "localhost:9092";
+
+    public static void main(String[] args) {
+        // Set up the producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // Create a new Kafka producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        //create weather station mock
+        WeatherStationMock station = new WeatherStationMock(1);
+        
+
+        while (true) {
+            // Create a weather status message
+            String message = station.generateStatusMessage();
+            
+            // drop message
+            if(message == null)
+                continue;
+
+            // Create a producer record with the message and send it to the Kafka topic
+            ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC, message);
+            producer.send(record);
+
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println(message);
+            
+        }
+
+        // Close the producer
+        //producer.close();
+        
+    }
+}
---
+++ src/main/java/org/example/WeatherStationMock.java
@@ -0,0 +1,75 @@
+package org.example;
+import com.google.gson.Gson;
+import java.util.Random;
+
+public class WeatherStationMock {
+    private final long stationId;
+    private long messageCounter;
+    private final Random random;
+    private final Gson gson;
+    
+    public WeatherStationMock(long stationId) {
+        this.stationId = stationId;
+        this.messageCounter = 0;
+        this.random = new Random();
+        this.gson = new Gson();
+    }
+    
+    public String generateStatusMessage() {
+        // Increment message counter
+        messageCounter++;
+        
+        // Generate weather data
+        int humidity = random.nextInt(101);
+        int temperature = random.nextInt(100) + 1; // Temperature is from 1 to 100 degrees Fahrenheit
+        int windSpeed = random.nextInt(61); // Wind speed is from 0 to 60 km/h
+        
+        // Generate battery status
+        String batteryStatus = "medium";
+        int batteryStatusRoll = random.nextInt(10) + 1;
+        if (batteryStatusRoll <= 3) {
+            batteryStatus = "low";
+        } else if (batteryStatusRoll >= 8) {
+            batteryStatus = "high";
+        }
+        
+        // Check if message should be dropped
+        if (random.nextInt(10) == 0) { // 10% chance of dropping message
+            return null;
+        }
+        
+        // Construct status message
+        long timestamp = System.currentTimeMillis() / 1000;
+        WeatherData weatherData = new WeatherData(humidity, temperature, windSpeed);
+        WeatherStatusMessage statusMessage = new WeatherStatusMessage(stationId, messageCounter, batteryStatus, timestamp, weatherData);
+        return gson.toJson(statusMessage);
+    }
+    
+    private static class WeatherStatusMessage {
+        private final long station_id;
+        private final long s_no;
+        private final String battery_status;
+        private final long status_timestamp;
+        private final WeatherData weather;
+        
+        public WeatherStatusMessage(long station_id, long s_no, String battery_status, long status_timestamp, WeatherData weather) {
+            this.station_id = station_id;
+            this.s_no = s_no;
+            this.battery_status = battery_status;
+            this.status_timestamp = status_timestamp;
+            this.weather = weather;
+        }
+    }
+    
+    private static class WeatherData {
+        private final int humidity;
+        private final int temperature;
+        private final int wind_speed;
+        
+        public WeatherData(int humidity, int temperature, int wind_speed) {
+            this.humidity = humidity;
+            this.temperature = temperature;
+            this.wind_speed = wind_speed;
+        }
+    }
+}
