208c961a0ae952f1b69f355c6df8f0e4c74a1006
moaz nabil <moaznabil886@gmail.com>
2023-05-27 08:05:55 +0300

    adds environment variable for k8s

--- src/main/java/org/example/WeatherStation.java
+++ src/main/java/org/example/WeatherStation.java
@@ -2,33 +2,59 @@ package org.example;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.LongSerializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 public class WeatherStation implements Runnable{
     private static final String TOPIC = "weather_data";
-    private static final String BOOTSTRAP_SERVERS = "localhost:9092";
-    private final int stationID;
+    private static long stationID;
 
-    public WeatherStation(int stationID) {
-        this.stationID = stationID;
+    public WeatherStation(long stationID) {
+        WeatherStation.stationID = stationID;
     }
 
     public static void main(String[] args) {
+        // Retrieve host and port from environment variables
+        String kafkaHost = System.getenv("KAFKA_HOST");
+        String kafkaPort = System.getenv("KAFKA_PORT");
+        String hostname = System.getenv("HOSTNAME");
+
+        System.out.println("KAFKA_HOST:" + kafkaHost + "\nKAFKA_PORT:" +kafkaPort + "\nHOSTNAME:" + hostname);
+        
+        if (kafkaHost == null) {
+            System.err.println("KAFKA_HOST environment variable not set");
+            System.exit(1);
+        }
+
+        if (kafkaPort == null) {
+            System.err.println("KAFKA_PORT environment variable not set");
+            System.exit(1);
+        }
+
+        if (hostname == null) {
+            System.err.println("HOSTNAME environment variable not set");
+            System.exit(1);
+        }
+
+        String BOOTSTRAP_SERVERS =  kafkaHost+':'+kafkaPort;
         // Set up the producer properties
         Properties properties = new Properties();
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
-        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
         // Create a new Kafka producer
-        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+        KafkaProducer<Long, String> producer = new KafkaProducer<>(properties);
 
         //create weather station mock
-
-        WeatherStationMock station = args.length>0?new WeatherStationMock(Integer.parseInt(args[0])):new WeatherStationMock(1);
+        Random rand = new Random(hostname.hashCode());
+        stationID = Math.abs(rand.nextLong());
+        System.out.println("===="+ stationID);
+        WeatherStationMock station = new WeatherStationMock(stationID);
         
 
         while (true) {
@@ -40,7 +66,7 @@ public class WeatherStation implements Runnable{
                 continue;
 
             // Create a producer record with the message and send it to the Kafka topic
-            ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC, message);
+            ProducerRecord<Long, String> record = new ProducerRecord<>(TOPIC,stationID,message);
             producer.send(record);
 
             try {
@@ -59,6 +85,6 @@ public class WeatherStation implements Runnable{
     }
     @Override
     public void run() {
-        main(new String[]{String.valueOf(this.stationID)});
+        main(new String[]{String.valueOf(WeatherStation.stationID)});
     }
 }
